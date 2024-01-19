e8e3fe0bb55e1c59a6d324342811148426cbc268
dara-rara <glebova555dasha@gmail.com>
2024-01-20 03:15:26 +0500

    add

--- project/staticfiles/script.js
+++
@@ -1,8 +0,0 @@
-let articleSections = document.querySelectorAll('.blog-article');
-
-for (let articleSection of articleSections) {
-  let showButton = articleSection.querySelector('button.more');
-  showButton.onclick = function() {
-    articleSection.classList.remove('short');
-  }
-}
